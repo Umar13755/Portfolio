98e660191e19c2920d91969ddb36873024aa54e6
Umar13755 <umarkhayyam570@gmail.com>
2024-11-12 07:48:18 -0800

    layout updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div className="mx-6">
+    // <div className="lg:mx-20 mx-4 ">
+    //   <Hero />
+    // </div>
+    <div className="lg:mx-20 mx-4 justify-center items-center min-h-screen">
       <Hero />
     </div>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,34 +4,56 @@ import { Button } from "./ui/Button";
 export default function Hero() {
   return (
     <>
-      <div className="absolute bottom-auto left-auto right-50 top-0 h-[400px] w-[400px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-30 blur-[80px]"></div>
-      <div className="absolute bottom-auto left-auto right-10 top-0 h-[400px] w-[400px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(160,160,160,0.5)] opacity-30 blur-[80px]"></div>
+      <div className="absolute bottom-auto left-auto right-50 top-0 h-[400px] w-[400px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(160,160,160,0.5)] opacity-30 blur-[80px]"></div>
+      <div className="absolute bottom-auto left-auto right-10 top-0 h-[400px] w-[400px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(160,160,160,0.5)]  opacity-30 blur-[80px]"></div>
 
-      <div className="flex items-center justify-center min-h-screen gap-3">
-        <div className="bg-black p-6 w-full rounded-3xl flex justify-between">
-          <div className="flex flex-row gap-4">
-            <img
-              src="/profile.svg"
-              className="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full"
-            />
-            <div>
-              <h2 className="font-semibold">Hey, I'm Umar.</h2>
-              <p className="font-medium text-sm text-[#9b9b9b]">
-                Software Engineer
-              </p>
+      <div className="grid lg:flex gird-cols-1 items-center justify-center my-10 gap-5">
+        {/* Profile Card */}
+        <div className="bg-black p-10 w-full rounded-[3rem]  grid grid-cols lg:sapce-y-0 space-y-3 ">
+          <div className="lg:flex lg:justify-between lg:space-y-0 space-y-4">
+            {/* left Section */}
+            <div className="flex flex-row gap-4">
+              <img
+                src="/profile.svg"
+                className="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full"
+              />
+              <div>
+                <h2 className="font-medium">Hey, I'm Khayyam.</h2>
+                <p className="font-medium text-sm text-[#9b9b9b]">
+                  Software Engineer
+                </p>
+              </div>
+            </div>
+
+            {/* Right Section */}
+            <div className="flex flex-row gap-2">
+              <Twitter className="icon" />
+              <Github className="icon" />
+              <Button className="" variant="outline" size="small">
+                Get In Touch
+              </Button>
             </div>
           </div>
 
-          <div className="flex flex-row gap-2">
-            <Twitter className="icon" />
-            <Github className="icon" />
-            <Button className="" variant="outline" size="small">Get In Touch</Button>
+          {/* Text Section */}
+          <div className="space-y-5 text-balance">
+            <h2 className="font-medium text-4xl">
+              Passionate about crafting seamless, modern interfaces with
+              innovative tech.
+            </h2>
+            <p className="text-sm font-normal text-pretty">
+              Hey, I'm <b>Umar Khayyam</b>, welcome to my world. I love building beautiful,
+              timeless websites & digital experiences with Framer. I have experience with Next Js & React JS for buidling modern web apps
+            </p>
           </div>
         </div>
 
-        <div className="bg-black p-5 w-2/4">
-          <h2>Hey, I'm Umar</h2>
-          <p>Software Engineer</p>
+        {/* Picture  */}
+        <div className="bg-black p-5 rounded-[3rem]">
+          <img
+            src="/profile.svg"
+            className="h-max w-96"
+          />
         </div>
       </div>
     </>
